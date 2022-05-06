2ac868d0bd679a89000f46e1f0c98733995473c4
Santiago Astini <santiastini95@gmail.com>
2022-05-06 17:19:55 -0300

    commit 04
    illus and works donde

--- assets/js/img-loader.js
+++ assets/js/img-loader.js
@@ -21,3 +21,27 @@ gsap.utils.toArray(".show-prj").forEach((el) => {
     el.addEventListener('mouseleave', () => fade.reverse());
    
   });
+
+gsap.utils.toArray(".show-i-prj").forEach((el) => {
+  
+    const image = el.querySelector('img.show-i-prj__sticky-img'),
+          setX = gsap.quickSetter(image, "x", "px"),
+          setY = gsap.quickSetter(image, "y", "px"),
+          align = e => {
+            const top = el.getBoundingClientRect().top;
+            setX(e.clientX );
+            setY(e.clientY - top);
+          },
+          startFollow = () => document.addEventListener("mousemove", align),
+          stopFollow = () => document.removeEventListener("mousemove", align),
+          fade = gsap.to(image, {autoAlpha: 1, ease: "none", paused: true, onReverseComplete: stopFollow});
+    
+    el.addEventListener('mouseenter', (e) => {
+      fade.play();
+      startFollow();
+      align(e);
+    });
+    
+    el.addEventListener('mouseleave', () => fade.reverse());
+   
+  });
